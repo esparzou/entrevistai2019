06c4ee09aace4aef4e8e20a14bbf773afe35c037
esparzou <esparzou@gmail.com>
2022-04-11 19:34:12 +0200

    change router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/entrevistai2019.git/'
+    base: '/entrevistai2019/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
